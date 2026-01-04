79f44ba3ce96e1b3e49aabd839714a05f379bce6
novak-stevanovic <novakst24@gmail.com>
2026-01-04 20:41:58 +0100

    ordered typedefs in ntg_typedef.h

--- include/shared/ntg_typedef.h
+++ include/shared/ntg_typedef.h
@@ -23,41 +23,54 @@ struct ntg_object_draw_ctx;
 struct ntg_object_draw_out;;
 
 /* -------------------------------------------------------------------------- */
-/* STRUCT, ENUM */
+/* STRUCT & ENUM */
 /* -------------------------------------------------------------------------- */
 
-// SHARED
+/* ------------------------------------------------------ */
+/* SHARED */
+/* ------------------------------------------------------ */
 
-/* Vector */
 typedef struct ntg_vec ntg_vec;
 
-typedef int (*ntg_vec_cmp_fn)(const void* data1, const void* data2);
-
-/* Status */
 typedef unsigned int ntg_status;
 
-/* XY */
 typedef enum ntg_orientation ntg_orientation;
 typedef enum ntg_alignment ntg_alignment;
 
-// CORE
+/* ------------------------------------------------------ */
+/* BASE */
+/* ------------------------------------------------------ */
 
-/* Loop */
+typedef struct ntg_cell_grid ntg_cell_grid;
+typedef struct ntg_vcell_grid ntg_vcell_grid;
+typedef struct ntg_cell_vecgrid ntg_cell_vecgrid;
+typedef struct ntg_vcell_vecgrid ntg_vcell_vecgrid;
+typedef enum ntg_vcell_type ntg_vcell_type;
+
+/* ------------------------------------------------------ */
+/* CORE */
+/* ------------------------------------------------------ */
+
+typedef struct ntg_entity ntg_entity;
+typedef struct ntg_entity_system ntg_entity_system;
+typedef struct ntg_entity_map ntg_entity_map;
+typedef struct ntg_event_observe_vec ntg_event_obs_vec;
+typedef struct ntg_entity_vec ntg_entity_vec;
+typedef struct ntg_const_entity_vec ntg_const_entity_vec;
+typedef struct ntg_entity_type ntg_entity_type;
+
 typedef struct ntg_loop ntg_loop;
 typedef struct ntg_loop_ctx ntg_loop_ctx;
 typedef enum ntg_loop_event_mode ntg_loop_event_mode;
 
-/* Renderer */
 typedef struct ntg_renderer ntg_renderer;
 typedef struct ntg_def_renderer ntg_def_renderer;
 
-/* Stage */
 typedef struct ntg_stage ntg_stage;
 typedef struct ntg_def_stage ntg_def_stage;
 typedef struct ntg_stage_drawing ntg_stage_drawing;
 typedef enum ntg_stage_event_mode ntg_stage_event_mode;
 
-/* Scene */
 typedef struct ntg_scene ntg_scene;
 typedef struct ntg_def_scene ntg_def_scene;
 typedef struct ntg_scene_graph ntg_scene_graph;
@@ -65,7 +78,6 @@ typedef struct ntg_focuser ntg_focuser;
 typedef struct ntg_single_focuser ntg_single_focuser;
 typedef enum ntg_scene_event_mode ntg_scene_event_mode;
 
-/* Object */
 typedef struct ntg_object ntg_object;
 typedef struct ntg_object_vec ntg_object_vec;
 typedef struct ntg_const_object_vec ntg_const_object_vec;
@@ -74,10 +86,7 @@ typedef struct ntg_object_size_map ntg_object_size_map;
 typedef struct ntg_object_xy_map ntg_object_xy_map;
 typedef struct ntg_object_drawing ntg_object_drawing;
 typedef struct ntg_object_map ntg_object_map;
-
-
 typedef struct ntg_label ntg_label;
-
 typedef struct ntg_color_block ntg_color_block;
 typedef struct ntg_prog_bar ntg_prog_bar;
 typedef struct ntg_box ntg_box;
@@ -87,42 +96,34 @@ typedef enum ntg_padding_enable_mode ntg_padding_enable_mode;
 typedef struct ntg_def_padding ntg_def_padding;
 typedef struct ntg_def_border ntg_def_border;
 
-// BASE
-
-/* Cell */
-
-typedef struct ntg_cell_grid ntg_cell_grid;
-typedef struct ntg_vcell_grid ntg_vcell_grid;
-typedef struct ntg_cell_vecgrid ntg_cell_vecgrid;
-typedef struct ntg_vcell_vecgrid ntg_vcell_vecgrid;
-typedef enum ntg_vcell_type ntg_vcell_type;
-
-/* Entity */
-typedef struct ntg_entity ntg_entity;
-typedef struct ntg_entity_system ntg_entity_system;
-typedef struct ntg_entity_map ntg_entity_map;
-typedef struct ntg_event_observe_vec ntg_event_obs_vec;
-typedef struct ntg_entity_vec ntg_entity_vec;
-typedef struct ntg_const_entity_vec ntg_const_entity_vec;
-typedef struct ntg_entity_type ntg_entity_type;
-
 /* -------------------------------------------------------------------------- */
-/* FN */
+/* FUNCTION */
 /* -------------------------------------------------------------------------- */
+
+/* ------------------------------------------------------ */
+/* SHARED */
+/* ------------------------------------------------------ */
+
+typedef int (*ntg_vec_cmp_fn)(const void* data1, const void* data2);
+
+/* ------------------------------------------------------ */
+/* BASE */
+/* ------------------------------------------------------ */
+
+typedef void (*ntg_entity_deinit_fn)(ntg_entity* entity);
+typedef void (*ntg_event_handler_fn)(ntg_entity* observer, struct ntg_event event);
+
+/* ------------------------------------------------------ */
+/* CORE */
+/* ------------------------------------------------------ */
+
+/* LOOP ------------------------------------------------- */
 
 typedef bool (*ntg_loop_event_fn)(
         ntg_loop_ctx* ctx,
         struct nt_event event);
 
-
-typedef void (*ntg_label_post_draw_fn)(
-        ntg_label* label,
-        ntg_object_drawing* out_drawing,
-        struct ntg_xy size,
-        sarena* arena);
-
-typedef void (*ntg_entity_deinit_fn)(ntg_entity* entity);
-typedef void (*ntg_event_handler_fn)(ntg_entity* observer, struct ntg_event event);
+/* OBJECT ----------------------------------------------- */
 
 typedef bool (*ntg_object_event_fn)(
         ntg_object* object,
@@ -177,6 +178,14 @@ typedef void (*ntg_object_draw_fn)(
         void* layout_data,
         sarena* arena);
 
+typedef void (*ntg_label_post_draw_fn)(
+        ntg_label* label,
+        ntg_object_drawing* out_drawing,
+        struct ntg_xy size,
+        sarena* arena);
+
+/* SCENE ------------------------------------------------ */
+
 /* Performs logical layout of the scene. */
 typedef void (*ntg_scene_layout_fn)(
         ntg_scene* scene,
@@ -193,11 +202,7 @@ typedef bool (*ntg_focuser_dispatch_fn)(
         struct nt_event event,
         ntg_loop_ctx* ctx);
 
-/* If `stage_drawing` is NULL, render empty */
-typedef void (*ntg_renderer_render_fn)(
-        ntg_renderer* renderer,
-        const ntg_stage_drawing* stage_drawing,
-        sarena* arena);
+/* STAGE ------------------------------------------------ */
 
 /* Composes the scene's logical drawing into an ntg_stage_drawing */
 typedef void (*ntg_stage_compose_fn)(
@@ -209,5 +214,13 @@ typedef bool (*ntg_stage_event_fn)(
         ntg_stage* stage,
         struct nt_event event,
         ntg_loop_ctx* loop_ctx);
+
+/* RENDERER --------------------------------------------- */
+
+/* If `stage_drawing` is NULL, render empty */
+typedef void (*ntg_renderer_render_fn)(
+        ntg_renderer* renderer,
+        const ntg_stage_drawing* stage_drawing,
+        sarena* arena);
 
 #endif // _NTG_TYPEDEF_H_
